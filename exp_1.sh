2fed0d935bef60b1a19eef21f76df9e79e511449
Sami-chabani <76627314+Sami-chabani@users.noreply.github.com>
2022-01-01 15:43:54 +0100

    Update exp_1.sh

--- exp_1.sh
+++ exp_1.sh
@@ -1,11 +1,12 @@
-# Experiment on an artificial event log - RQ1, RQ2 with proposed method
+#Experiment on real event shown in Fig.6 and Fig.7 with proposed approach
+
+# Each file contains different number of instances for resource allocation
+test_path_array=("./sample_data/artificial/testlog_0806_1_40.csv" "./sample_data/artificial/testlog_0806_1_60.csv" "./sample_data/artificial/testlog_0806_1_80.csv" "./sample_data/artificial/testlog_0806_1_100.csv" "./sample_data/artificial/testlog_0806_1_120.csv" "./sample_data/artificial/testlog_0806_1_140.csv""./sample_data/artificial/testlog_0806_1_180.csv")
 
 # Threshold for the prediction accuracy
 threshold=(0.3)
 
-# Each file contains different number of instances for resource allocation
-# test_path_array=("./sample_data/artificial/testlog_0806_1_40.csv" "./sample_data/artificial/testlog_0806_1_60.csv" "./sample_data/artificial/testlog_0806_1_80.csv" "./sample_data/artificial/testlog_0806_1_100.csv" "./sample_data/artificial/testlog_0806_1_120.csv" "./sample_data/artificial/testlog_0806_1_140.csv")
-test_path_array=("./sample_data/artificial/testlog_0806_1_90.csv")
+
 
 for alpha in ${threshold[@]}; do
 	for test_path in ${test_path_array[@]}; do
